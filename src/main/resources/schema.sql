3ea6b3513c3221e5ad3bd5a4e40c60d865e194d4
vslobo <antilamer1709@gmail.com>
2020-10-22 13:46:24 +0300

    new Statuses

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -2,13 +2,13 @@ DROP TABLE PUBLIC.task IF EXISTS;
 CREATE TABLE PUBLIC.task  (
     id BIGINT IDENTITY NOT NULL PRIMARY KEY,
     status_id BIGINT NOT NULL,
-    name VARCHAR(20)
+    name VARCHAR(64)
 );
 
 DROP TABLE PUBLIC.status IF EXISTS;
 CREATE TABLE PUBLIC.status  (
     id BIGINT IDENTITY NOT NULL PRIMARY KEY,
-    name VARCHAR(20)
+    name VARCHAR(32)
 );
 
 alter table PUBLIC.task
@@ -18,5 +18,12 @@ alter table PUBLIC.task
 
 
 INSERT INTO PUBLIC.status (id, name) VALUES (1, 'New');
+INSERT INTO PUBLIC.status (id, name) VALUES (2, 'Ready For Development');
+INSERT INTO PUBLIC.status (id, name) VALUES (3, 'In Development');
+INSERT INTO PUBLIC.status (id, name) VALUES (4, 'Ready For Test');
+INSERT INTO PUBLIC.status (id, name) VALUES (5, 'In Test');
+INSERT INTO PUBLIC.status (id, name) VALUES (6, 'Pending');
+INSERT INTO PUBLIC.status (id, name) VALUES (7, 'Rejected');
+INSERT INTO PUBLIC.status (id, name) VALUES (8, 'Resolved');
 
 INSERT INTO PUBLIC.task (status_id, name) VALUES (1, 'Important task');
